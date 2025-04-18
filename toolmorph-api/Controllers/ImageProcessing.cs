16a06be4de9fe8375148b2cd01f23a6748738f4d
ByatisGlaaki <rares_famuntean@yahoo.com>
2025-04-18 17:17:45 +0300

    Add project files.

---
+++ toolmorph-api/Controllers/ImageProcessing.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace toolmorph_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageProcessing : ControllerBase
+    {
+        [HttpPost("extract-palette")]
+        public IEnumerable<string> extractPalette([FromForm] IFormFile file)
+        {
+            // Actual call to python api service.
+            return new List<string> { "Color1", "Color2", "Color3" };
+        }
+    }
+}
